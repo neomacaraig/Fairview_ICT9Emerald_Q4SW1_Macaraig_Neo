9eae43ca55ea1a2ecde47aab520f77e8a0e17596
neomacaraig <macaraig.na@obmontessori.edu.ph>
2026-03-26 09:05:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 function toggleInstructions() {
-var x = document.getElementById("instructions");
+    var instr = document.getElementById("instructions");
 
-if (x.style.display == "none") {
-x.style.display = "block";
-} else {
-x.style.display = "none";
-}
+    if (instr.style.display == "none") {
+        instr.style.display = "block";
+    } else {
+        instr.style.display = "none";
+    }
 }
 
+// shows success or failed when person logs in
 function login() {
-var user = document.getElementById("username").value;
-var pass = document.getElementById("password").value;
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
 
-if (user.length >= 4 && pass.length >= 6) {
-document.getElementById("output").innerHTML = "Login Successful!";
-alert("Welcome " + user);
-} else {
-document.getElementById("output").innerHTML = "Invalid input!";
-}
+    if (user.length >= 4 && pass.length >= 6) {
+        document.getElementById("output").innerHTML = "Login Successful!";
+        alert("Welcome " + user);
+    } else {
+        document.getElementById("output").innerHTML = "Invalid input!";
+    }
 }
 
 function showPass() {
-var x = document.getElementById("password");
+    var x = document.getElementById("password");
 
-if (x.type == "password") {
-x.type = "text";
-} else {
-x.type = "password";
-}
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
 }
